1f4d614a2fa51541784fcfeabe14ab497ffcde08
quinterojoel <157430955+quinterojoel@users.noreply.github.com>
2025-03-07 13:53:45 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,16 @@
 def multiply(n):
     #Add the code and an explanation on how to 
     #multiply a number by 2 using bitshift
-    return n
+    #Convert to binary
+    #Divide by 2 to find remainders
+    #Print remainders into a list
+    #Append a 0 at the end
 
+list = []
+n = 13
+while n is not 0 or 1:
+    n = n / 2
+list.append(n)
+list.append(0)
+
+    return n
